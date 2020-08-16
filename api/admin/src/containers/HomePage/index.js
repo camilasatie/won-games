e53da2b862262c3f9e4c1cb6ffe65a0b902d18d0
camilasatie <camilasatie.dev@gmail.com>
2020-08-16 12:22:59 -0300

    chore: customize home

---
+++ api/admin/src/containers/HomePage/index.js
@@ -0,0 +1,141 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+import React, { memo } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { get } from 'lodash';
+import { auth } from 'strapi-helper-plugin';
+import PageTitle from '../../components/PageTitle';
+
+import { ALink, Block, Container, LinkWrapper, P, Separator } from './components';
+import SocialLink from './SocialLink';
+
+const FIRST_BLOCK_LINKS = [
+  {
+    link:
+      'https://strapi.io/documentation/v3.x/getting-started/quick-start.html#_4-create-a-category-content-type',
+    contentId: 'app.components.BlockLink.documentation.content',
+    titleId: 'app.components.BlockLink.documentation',
+  },
+  {
+    link: 'https://github.com/strapi/foodadvisor',
+    contentId: 'app.components.BlockLink.code.content',
+    titleId: 'app.components.BlockLink.code',
+  },
+];
+
+const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    link: 'https://github.com/React-avancado/',
+  },
+  {
+    name: 'Slack',
+    link: 'https://bit.ly/will-slack/',
+  }
+];
+
+const HomePage = ({ global: { plugins }, history: { push } }) => {
+  const handleClick = e => {
+    e.preventDefault();
+
+    push(
+      '/plugins/content-type-builder/content-types/plugins::users-permissions.user?modalType=contentType&kind=collectionType&actionType=create&settingType=base&forTarget=contentType&headerId=content-type-builder.modalForm.contentType.header-create&header_icon_isCustom_1=false&header_icon_name_1=contentType&header_label_1=null'
+    );
+  };
+  const hasAlreadyCreatedContentTypes =
+    get(plugins, ['content-manager', 'leftMenuSections', '0', 'links'], []).filter(
+      contentType => contentType.isDisplayed === true
+    ).length > 1;
+
+  const headerId = hasAlreadyCreatedContentTypes
+    ? 'HomePage.greetings'
+    : 'app.components.HomePage.welcome';
+  const username = get(auth.getUserInfo(), 'username', '');
+  const linkProps = hasAlreadyCreatedContentTypes
+    ? {
+        id: 'app.components.HomePage.button.blog',
+        href: 'https://strapi.io/blog/',
+        onClick: () => {},
+        type: 'blog',
+        target: '_blank',
+      }
+    : {
+        id: 'app.components.HomePage.create',
+        href: '',
+        onClick: handleClick,
+        type: 'documentation',
+      };
+
+  return (
+    <>
+      <FormattedMessage id="HomePage.helmet.title">
+        {title => <PageTitle title={title} />}
+      </FormattedMessage>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8 col-md-12">
+            <Block>
+              <h2 id="mainHeader">Bem vindo a Won Games</h2>
+              <P>Ao lado você pode inserir diferentes jogos, categorias e publishers para a nosa maravilhosa loja de jogos!</P>
+              <Separator style={{ marginTop: 37, marginBottom: 36 }} />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {FIRST_BLOCK_LINKS.map((data, index) => {
+                  const type = index === 0 ? 'doc' : 'code';
+
+                  return (
+                    <LinkWrapper href={data.link} target="_blank" key={data.link} type={type}>
+                      <FormattedMessage id={data.titleId}>
+                        {title => <p className="bold">{title}</p>}
+                      </FormattedMessage>
+                      <FormattedMessage id={data.contentId}>
+                        {content => <p>{content}</p>}
+                      </FormattedMessage>
+                    </LinkWrapper>
+                  );
+                })}
+              </div>
+            </Block>
+          </div>
+
+          <div className="col-md-12 col-lg-4">
+            <Block style={{ paddingRight: 30, paddingBottom: 0 }}>
+              <h2>Veja nossos links</h2>
+              <P style={{ marginTop: 7, marginBottom: 0 }}>Em caso de dúvidas ou sugestões, basta acessar um de nossos links abaixo.</P>
+              <FormattedMessage id="HomePage.roadmap">
+
+                  <ALink
+                    rel="noopener noreferrer"
+                    href="https://reactavancado.com.br"
+                    target="_blank"
+                  >Veja o nosso planejamento de módulos.
+                  </ALink>
+
+              </FormattedMessage>
+
+              <Separator style={{ marginTop: 18 }} />
+              <div
+                className="row social-wrapper"
+                style={{
+                  display: 'flex',
+                  margin: 0,
+                  marginTop: 36,
+                  marginLeft: -15,
+                }}
+              >
+                {SOCIAL_LINKS.map((value, key) => (
+                  <SocialLink key={key} {...value} />
+                ))}
+              </div>
+            </Block>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
